fd0c0d0eb9ba5b7defc0493e2e1ba315e6750d47
itsmedanish <danish.khan@tylertech.com>
2016-11-03 10:50:14 -0700

    Calendar control

---
+++ CodeManProject/SchedulerPage/TFSHelper.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+// https://www.nuget.org/packages/Microsoft.TeamFoundationServer.Client/
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+
+//https://www.nuget.org/packages/Microsoft.TeamFoundationServer.Client/14.89.0
+using Microsoft.TeamFoundation.Client;
+
+// https://www.nuget.org/packages/Microsoft.VisualStudio.Services.InteractiveClient/
+using Microsoft.VisualStudio.Services.Client;
+
+// https://www.nuget.org/packages/Microsoft.VisualStudio.Services.Client/
+using Microsoft.VisualStudio.Services.Common;
+
+using Microsoft.TeamFoundation.Framework.Client;
+using Microsoft.TeamFoundation.Framework.Common;
+
+namespace CodeManProject
+{
+    public class TFSHelper
+    {
+
+        public string TFSURL { get; set; }
+        public string TFSProject { get; set; }
+        public List<WorkItem> getWorkItems()
+        {
+            // Create a connection object, which we will use to get httpclient objects.  This is more robust
+            // then newing up httpclient objects directly.  Be sure to send in the full collection uri.
+            // For example:  http://myserver:8080/tfs/defaultcollection
+            VssCredentials vsCred = new VssCredentials(true);
+            VssConnection connection = new VssConnection(new Uri(TFSURL), vsCred);
+
+            // Create instance of WorkItemTrackingHttpClient using VssConnection
+            WorkItemTrackingHttpClient witClient = connection.GetClient<WorkItemTrackingHttpClient>();
+
+            List<WorkItem> resultWorkItem = new List<WorkItem>();
+
+            try
+            { 
+                // Get 2 levels of query hierarchy items
+                List<QueryHierarchyItem> queryHierarchyItems = witClient.GetQueriesAsync(TFSProject, depth: 0).Result;
+
+                // Search for 'My Queries' folder
+                QueryHierarchyItem myQueriesFolder = queryHierarchyItems.FirstOrDefault(qhi => qhi.Name.Equals("My Queries"));
+                resultWorkItem = new List<WorkItem>();
+                string queryStr = "SELECT [System.Id],[System.WorkItemType],[System.Title],[System.AssignedTo],[System.State],[System.Tags] FROM WorkItems WHERE [System.State] = 'New'";
+                Wiql wiql = new Wiql();
+                wiql.Query = queryStr;
+
+                
+                
+
+                WorkItemQueryResult result = witClient.QueryByWiqlAsync(wiql).Result;
+                //witClient.GetQueryAsync(TFSProject, queryStr).Result;
+                if (result.WorkItems.Any())
+                {
+                    int skip = 0;
+                    const int batchSize = 100;
+                    IEnumerable<WorkItemReference> workItemRefs;
+                    do
+                    {
+                        workItemRefs = result.WorkItems.Skip(skip).Take(batchSize);
+                        if (workItemRefs.Any())
+                        {
+                            // get details for each work item in the batch
+                            List<WorkItem> workItems = witClient.GetWorkItemsAsync(workItemRefs.Select(wir => wir.Id)).Result;
+                            foreach (WorkItem workItem in workItems)
+                            {
+                                // write work item to console
+                                resultWorkItem.Add(workItem);
+                            }
+                        }
+                        skip += batchSize;
+                    }
+                    while (workItemRefs.Count() == batchSize);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return resultWorkItem;
+        }
+
+        public List<String> getUsers()
+        {
+            string groupName = String.Format(@"%1\Contributors", TFSProject);
+            List<string> userList = new List<string>();
+
+            TfsTeamProjectCollection tfs = new TfsTeamProjectCollection(new Uri(TFSURL));
+            tfs.EnsureAuthenticated();
+            IIdentityManagementService ims = tfs.GetService<IIdentityManagementService>();
+
+            TeamFoundationIdentity rootIdentity = ims.ReadIdentity(IdentitySearchFactor.AccountName, groupName, MembershipQuery.Direct, ReadIdentityOptions.None);
+
+            DisplayGroupTree(ims, rootIdentity, 0, userList);
+
+            return userList;
+        }
+
+        private static void DisplayGroupTree(IIdentityManagementService ims, TeamFoundationIdentity node, int level, List<string> usrLst)
+        {
+            DisplayNode(node, level, usrLst);
+
+            if (!node.IsContainer)
+                return;
+
+            TeamFoundationIdentity[] nodeMembers = ims.ReadIdentities(node.Members, MembershipQuery.Direct,
+                ReadIdentityOptions.None);
+
+            int newLevel = level + 1;
+            foreach (TeamFoundationIdentity member in nodeMembers)
+                DisplayGroupTree(ims, member, newLevel, usrLst);
+        }
+
+        private static void DisplayNode(TeamFoundationIdentity node, int level, List<string> usrLst)
+        {
+            for (int tabCount = 0; tabCount < level; tabCount++)
+
+                usrLst.Add(node.DisplayName);
+        }
+    }
+}
---
+++ CodeManProject/SchedulerPage/Test.aspx
@@ -0,0 +1,108 @@
+﻿
+<%@ Page Language="C#" AutoEventWireup="true" Inherits="Test" CodeFile="Test.aspx.cs"  %>
+
+ 
+
+
+
+<%@ Register TagPrefix="telerik" Namespace="Telerik.Web.UI" Assembly="Telerik.Web.UI" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<html xmlns='http://www.w3.org/1999/xhtml'>
+
+<head runat="server">
+
+    <title>Telerik ASP.NET Example</title>
+
+</head>
+
+<body>
+
+    <form id="form1" runat="server">
+
+    <telerik:RadScriptManager runat="server" ID="RadScriptManager1" />
+
+    <telerik:RadSkinManager ID="RadSkinManager1" runat="server"   ShowChooser="true" />
+
+    <telerik:RadAjaxManager ID="RadAjaxManager1" runat="server">
+
+        <AjaxSettings>
+
+            <telerik:AjaxSetting AjaxControlID="RadScheduler1">
+
+                <UpdatedControls>
+
+                    <telerik:AjaxUpdatedControl ControlID="RadScheduler1" LoadingPanelID="RadAjaxLoadingPanel1">
+
+                    </telerik:AjaxUpdatedControl>
+
+                </UpdatedControls>
+
+            </telerik:AjaxSetting>
+
+        </AjaxSettings>
+
+    </telerik:RadAjaxManager>
+
+    <telerik:RadAjaxLoadingPanel ID="RadAjaxLoadingPanel1" runat="server">
+
+    </telerik:RadAjaxLoadingPanel>
+
+    <div class="demo-container no-bg">
+
+        <telerik:RadScheduler RenderMode="Lightweight" runat="server" ID="RadScheduler1" DayStartTime="08:00:00" 
+
+            DayEndTime="18:00:00" OnAppointmentInsert="RadScheduler1_AppointmentInsert" 
+              AllowEdit ="false"   ExportSettings-OpenInNewWindow="true"
+             OnAppointmentContextMenuItemClicked="RadScheduler1_AppointmentClick" ExportSettings-Pdf-PageTitle="test pdf"
+             
+            
+
+            OnAppointmentUpdate="RadScheduler1_AppointmentUpdate" OnAppointmentDelete="RadScheduler1_AppointmentDelete" 
+            OnAppointmentClick="RadScheduler1_AppointmentClick" 
+
+            DataKeyField="ID" DataSubjectField="Subject" DataStartField="Start" DataEndField="End"
+
+            DataRecurrenceField="RecurrenceRule" DataRecurrenceParentKeyField="RecurrenceParentId"
+
+            DataReminderField="Reminder">
+
+            <ExportSettings OpenInNewWindow="true" FileName="SchedulerExport">
+    <Pdf 
+        PageTitle="Schedule" 
+        Author="Telerik"
+
+        PaperSize="A5"
+        PageLeftMargin="40mm"
+        PageRightMargin="40mm"
+        PageBottomMargin="40mm"
+        PageTopMargin="40mm"
+
+        Creator="Telerik" 
+        Title="Schedule">
+    </Pdf>                    
+</ExportSettings>
+</telerik:RadScheduler>
+
+
+
+            <AdvancedForm Modal="true"></AdvancedForm>
+
+            <TimelineView UserSelectable="false"></TimelineView>
+
+            <TimeSlotContextMenuSettings EnableDefault="true"></TimeSlotContextMenuSettings>
+
+            <AppointmentContextMenuSettings EnableDefault="true"></AppointmentContextMenuSettings>
+
+            <Reminders Enabled="true"></Reminders>
+
+        </telerik:RadScheduler>
+
+    </div>
+
+    </form>
+
+</body>
+
+</html>
